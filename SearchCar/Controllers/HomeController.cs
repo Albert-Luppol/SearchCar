d0f652eed7263162deacae8b8d1e95471262c078
Альберт Луппол <luppol@live.ru>
2018-07-11 18:32:30 +0300

    Add project files.

---
+++ SearchCar/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+using SearchCar.Models;
+
+namespace SearchCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.Message = "Add a new car:";
+
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            var connString = "Server=127.0.0.1;Port=5432;Database=Cars;User Id=postgres;Password=sYk2f3Tn;";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+            }
+        
+        List<Car> cars = new List<Car>();
+            cars.Add(new Car { Make = "Lotus", Model = "Elise", Fuel = "Petrol", Year = 2017, EngineSize = 2.6, Millage = 10000 });
+            cars.Add(new Car { Make = "Lotus", Model = "Exige", Fuel = "Petrol", Year = 2017, EngineSize = 2.6, Millage = 10000 });
+            cars.Add(new Car { Make = "Mersedes", Model = "E260", Fuel = "Diesel", Year = 2012, EngineSize = 3.0, Millage = 105000 });
+            return View(cars);
+        }
+    }
+}
---
+++ SearchCar/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SearchCar.Models
+{
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Fuel { get; set; }
+        public int Year { get; set; }
+        public double EngineSize { get; set; }
+        public int Millage { get; set; }
+
+    }
+}
